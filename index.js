be09d14b69475304275fb3edb741da899f755b4e
Maitre-Pangolin <martin.wasselet@gmail.com>
2021-04-28 10:42:17 -0400

    Add procfile and basic express server functionnality
    Use for first deployment

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
+const express = require("express");
+const http = require("http");
+const { runInNewContext } = require("vm");
+const app = express();
+const server = http.createServer(app);
+const PORT = process.env.PORT || 3000;
+//const io = require("socket.io")(server);
 
+app.get("/", (req, res) => {
+  res.send("Homepage right here");
+});
+
+server.listen(PORT, () => console.log(`listening on port:${PORT}`));
