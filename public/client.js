2ac6c046f93adb2a0a2f9bf72ca1e1ec3617e935
Maitre-Pangolin <martin.wasselet@gmail.com>
2021-04-29 16:14:52 -0400

    Modify front-end

--- public/client.js
+++ public/client.js
@@ -1,20 +1,38 @@
 let socket = io();
 
-let chatform = document.getElementById("chat-form");
+//const chat = document.getElementById("chat");
+let chatForm = document.getElementById("chat-form");
+const loggingForm = document.getElementById("logging-form");
 let input = document.getElementById("input");
+const inputUser = document.getElementById("input-username");
 const messages = document.getElementById("messages");
+const userNameDiv = document.getElementById("username");
 let username = "";
 
-chatform.addEventListener("submit", (event) => {
+chatForm.addEventListener("submit", (event) => {
   event.preventDefault();
   if (input.value) {
-    socket.emit("chat message", input.value);
+    socket.emit("chat message", {
+      user: username,
+      message: input.value,
+    });
     input.value = "";
   }
 });
 
-socket.on("broadcast message", (data) => {
+loggingForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+  if (inputUser.value) {
+    loggingForm.style.display = "none";
+    console.log(inputUser.value);
+    username = inputUser.value;
+    userNameDiv.textContent = inputUser.value;
+  }
+});
+
+socket.on("broadcast message", ({ user, message }) => {
   const newMessage = document.createElement("li");
-  newMessage.textContent = data;
+  newMessage.textContent = user + ":   " + message;
   messages.appendChild(newMessage);
+  window.scrollTo(0, document.body.scrollHeight);
 });
